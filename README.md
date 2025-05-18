00d6b74525250c67967ec3aec6e603ea5e52fb07
efsotr <efsotr_l@stu.pku.edu.cn>
2025-05-18 23:21:32 +0800

    add all

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# nano-patch-pack
-Just a few lines to unlock Flash Attention 2 in Transformers – simple, clean, fast.
+# 🚀 nano-patch-sequence-pack
+
+Just a few lines to combine 🤗 **Transformers**, **Flash Attention 2**, and `torch.compile` — simple, clean, fast ⚡
+
+---
+
+All of them now support **sequence packing** — removing unnecessary padding by packing a batch of tokens into one contiguous sequence.
+But few frameworks make it easy to actually use.
+
+**Not anymore!** With a tiny `patch.py`, you can enable packing and compiler-level optimization in just **two lines** — and seamlessly plug it into **any framework built on Transformers** 💡
+
+```python
+from patch import patch
+patch(model)  # model loaded from Hugging Face Transformers
+```
+
+The logic inside `patch.py` is **clean and compact** — perfect for quick customization and extension.
+
+Example from [Enhancing SFT Training Efficiency Using Packing and FlashAttention2 with Position IDs](https://github.com/huggingface/transformers/pull/31629)
+
+
+### 📊 Example Result 1
+
+**Dataset**: OrcaMath subset
+
+**Setup**: FSDP with 8 GPUs
+
+| Model      | Data Process | Time (s) | Throughput (token/s) | Memory (MB) |
+| ---------- | ------------ | -------- | -------------------- | ----------- |
+| Llama2-7B  | Padding      | 790      | 1269                 | 22305       |
+| Llama2-7B  | ThisPR       | 574      | 1746                 | 20950       |
+| Mistral-7B | Padding      | 812      | 1216                 | 23603       |
+| Mistral-7B | ThisPR       | 596      | 1658                 | 22409       |
+
+
+### 📊 Example Result 2
+
+**Dataset**: FLAN subset
+
+**Setup**: FSDP with 8 GPUs
+
+| Model      | Data Process | Time (s) | Throughput (token/s) | Memory (MB) |
+| ---------- | ------------ | -------- | -------------------- | ----------- |
+| Llama2-7B  | Padding      | 1526     | 771                  | 29234       |
+| Llama2-7B  | ThisPR       | 809      | 1455                 | 23854       |
+| Mistral-7B | Padding      | 742      | 742                  | 30625       |
+| Mistral-7B | ThisPR       | 1408     | 1408                 | 24549       |
---
+++ patch.py
@@ -0,0 +1,72 @@
+import torch
+
+def wrap(fn):
+    
+    def wrapped_fn(input_ids, attention_mask, position_ids=None, **kwargs):
+        assert attention_mask is not None
+        assert not kwargs.get("output_hidden_states", False)
+        
+        attention_mask = attention_mask.bool()
+
+        # prepare shape
+        B, L = input_ids.shape
+        input_ids = input_ids[attention_mask][None] # (1, T)
+
+        # prepare position ids
+        if position_ids is None:
+            position_ids = torch.arange(0, L, device=attention_mask.device)[None].repeat(B, 1)
+        position_ids = position_ids[attention_mask][None]
+
+        indices = torch.nonzero(attention_mask.view(-1), as_tuple=True)[0]
+
+        # prepare flash attention 2 kwargs for transformers models
+        seqlens_in_batch = attention_mask.sum(dim=-1, dtype=torch.int32)
+        max_seqlen_in_batch = seqlens_in_batch.max().item()
+        cu_seqlens = torch.nn.functional.pad(torch.cumsum(seqlens_in_batch, dim=0, dtype=torch.int32), (1, 0), value=0)
+        attention_mask = None
+        kwargs["cu_seq_lens_q"] = cu_seqlens
+        kwargs["cu_seq_lens_k"] = cu_seqlens
+        kwargs["max_length_q"] = max_seqlen_in_batch
+        kwargs["max_length_k"] = max_seqlen_in_batch
+
+        output = fn(input_ids, attention_mask, position_ids, **kwargs) # original forward
+
+        # restore original shape 
+        last_hidden_state = output.last_hidden_state # (1, T, H)
+        output.last_hidden_state = torch.zeros((B * L, last_hidden_state.size(-1)), 
+                                               dtype=last_hidden_state.dtype,
+                                               device=last_hidden_state.device)
+        output.last_hidden_state[indices] = last_hidden_state[0] # (T, H)
+        output.last_hidden_state = output.last_hidden_state.view(B, L, -1)
+
+        return output
+
+    return wrapped_fn
+
+def enable_nonsac(enable_remove_add=True):
+    import torch._functorch.config
+    import torch._functorch.partitioners as partitioners
+
+    # reduce activation memory footprint 
+    torch._functorch.config.activation_memory_budget = 0.99
+
+    if enable_remove_add:
+        # Remove 'add' from the default operation list to eliminate redundant re-computation 
+        # due to wrong partitioning of nodes
+        def remove_add(fn):
+            def wrapped_fn():
+                optypes = fn()
+                optypes.recomputable_ops.remove(torch.ops.aten.add)
+                return optypes
+            return wrapped_fn
+
+        partitioners.get_default_op_list = remove_add(partitioners.get_default_op_list)
+
+def patch(model):
+    try:
+        enable_nonsac()
+    except Exception as e:
+        print("enable error", repr(e), flush=True)
+    model.model.forward = wrap(model.model.forward)
+    for layer in model.model.layers:
+        layer.forward = torch.compile(layer.forward, dynamic=True)
