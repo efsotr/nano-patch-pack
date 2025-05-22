6647c7d4308ef25a4875127cbdd28221dbc1319e
efsotr <efsotr_l@stu.pku.edu.cn>
2025-05-22 11:02:48 +0800

    fix errors when call path multi times

--- patch.py
+++ patch.py
@@ -43,24 +43,29 @@ def wrap(fn):
 
     return wrapped_fn
 
+nonsac_enable = False
+
 def enable_nonsac(enable_remove_add=True):
-    import torch._functorch.config
-    import torch._functorch.partitioners as partitioners
+    if not nonsac_enable:
+        import torch._functorch.config
+        import torch._functorch.partitioners as partitioners
 
-    # reduce activation memory footprint 
-    torch._functorch.config.activation_memory_budget = 0.99
+        nonsac_enable = True
 
-    if enable_remove_add:
-        # Remove 'add' from the default operation list to eliminate redundant re-computation 
-        # due to wrong partitioning of nodes
-        def remove_add(fn):
-            def wrapped_fn():
-                optypes = fn()
-                optypes.recomputable_ops.remove(torch.ops.aten.add)
-                return optypes
-            return wrapped_fn
+        # reduce activation memory footprint 
+        torch._functorch.config.activation_memory_budget = 0.99
 
-        partitioners.get_default_op_list = remove_add(partitioners.get_default_op_list)
+        if enable_remove_add:
+            # Remove 'add' from the default operation list to eliminate redundant re-computation 
+            # due to wrong partitioning of nodes
+            def remove_add(fn):
+                def wrapped_fn():
+                    optypes = fn()
+                    optypes.recomputable_ops.remove(torch.ops.aten.add)
+                    return optypes
+                return wrapped_fn
+
+            partitioners.get_default_op_list = remove_add(partitioners.get_default_op_list)
 
 def patch(model):
     try:
